23c0a6db485a17f91f77786d9989a7702c23fdd1
Hussain Tamboli <hussain@screen-magic.com>
2016-06-07 16:37:22 +0530

    With complete dependencies

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # Installation
 
 1. Install system dependencies
+
 $ sudo apt-get install tesseract-ocr
 $ sudo apt-get install python-opencv
 2. Download Opencv from http://opencv.org/downloads.html
-3. Follow these instructions to install opencv
-http://stackoverflow.com/questions/15790501/why-cv2-so-missing-after-opencv-installed
+
+3. Follow these instructions to install opencv http://stackoverflow.com/questions/15790501/why-cv2-so-missing-after-opencv-installed
+
 Set PYTHONPATH like it's said
 PYTHONPATH=~/projects/opencv/release/lib:$PYTHONPATH
+
 4. Install python dependencies
+
 pip install -r requirements.txt
+
+5. Test. It will recognise text from test.jpg
+python recognise.py
 
 
--- recognise.py
+++ recognise.py
@@ -5,7 +5,7 @@
 
 from PIL import Image, ImageFilter, ImageChops
 from pytesseract import image_to_string
-#import cv2
+import cv2
 
 def preprocess_image_using_pil(image_path):
     # unblur, sharpen filters
@@ -41,7 +41,6 @@ def preprocess_image_using_pil(image_path):
     wpercent = (basewidth/float(im_orig.size[0]))
     hsize = int((float(im_orig.size[1])*float(wpercent)))
     big = img.resize((basewidth, hsize), Image.ANTIALIAS)
-    #big = im_orig.resize((250, 100), Image.NEAREST)
 
     # tesseract-ocr only works with TIF so save the bigger image in that format
     ext = ".tif"
@@ -54,11 +53,9 @@ def preprocess_image_using_pil(image_path):
 def get_captcha_text_from_captcha_image(captcha_path):
     # Preprocess the image befor OCR
     tif_file = preprocess_image_using_opencv(captcha_path)
-    #   Perform OCR using tesseract-ocr library
-    # OCR : Optical Character Recognition
+    # Perform OCR using tesseract-ocr library
     image = Image.open(tif_file)
     ocr_text = image_to_string(image, config="-psm 6")
-    #alphanumeric_text = ''.join(e for e in ocr_text if e.isalnum())
     alphanumeric_text = ''.join(e for e in ocr_text)
 
     return alphanumeric_text
@@ -84,34 +81,6 @@ def binarize_image_using_opencv(captcha_path, binary_image_path='input-black-n-w
     im_bw = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)[1]
     cv2.imwrite(binary_image_path, im_bw)
     return binary_image_path
-
-
-# experimental
-# TODO - remove later
-def trim(im, cropped_image_path='cropped.gif'):
-    bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
-    diff = ImageChops.difference(im, bg)
-    diff = ImageChops.add(diff, diff, 2.0, -100)
-    bbox = diff.getbbox()
-    if bbox:
-        return im.crop(bbox)
-    im.save(cropped_image_path)
-    return cropped_image_path
-
-
-# experimental
-# TODO - remove later
-def autocrop(tif_image, threshold=0):
-    img = cv2.imread(tif_image)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnt = contours[0]
-    x,y,w,h = cv2.boundingRect(cnt)
-    crop = img[y:y+h,x:x+w]
-    cropped_tif = 'cropped.tif'
-    cv2.imwrite(cropped_tif, crop)
-    return cropped_tif
 
 
 def preprocess_image_using_opencv(captcha_path):
